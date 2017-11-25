55568782999684fcca2c210e2c6d8973294f5289
Fan <incomparable9527@foxmail.com>
2017-11-25 21:42:33 +0800

    删除无用代码

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,10 +31,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-var fs = require('fs')
 var path = require("path");
-// const NodeID3 = require('node-id3')
-// const id3_reader = require('id3_reader')
 var jsmediatags = require("jsmediatags");
 var axios = require("axios")
 var fs = require('fs')
@@ -140,10 +132,8 @@ function readFile(readurl,name){
 
 async function  getLrc(oFile,keywords){
   let lrc;
-  let params ={keywords,type:1}
-  
   //获取歌曲信息
-  let song = await axios.get('http://localhost:3000/search',{params})
+  let song = await axios.get('http://localhost:3000/search',{params:{keywords,type:1}})
   if(song.data.code==200){
 
     //选择符合度最高的
